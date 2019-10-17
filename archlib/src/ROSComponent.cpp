9b3d8d3450a40caafa4c4efc428290bfdecc7059
Gastd <gabriel.araujo.5000@gmail.com>
2019-10-17 17:02:45 -0300

    Handle the namespace with slash '/'

--- archlib/src/ROSComponent.cpp
+++ archlib/src/ROSComponent.cpp
@@ -25,10 +25,19 @@ namespace arch {
     std::string ROSComponent::getRosNodeName(const std::string& node_name, const std::string& node_namespace) {
         std::string ros_node_name = node_name;
 
-        // std::cout << ros_node_name << std::endl;
-        // ROS_INFO("%s", ros_node_name.c_str());
-        size_t pos = ros_node_name.find(node_namespace);
-        ros_node_name.replace(pos, node_namespace.length(), "");
+        std::cout << ros_node_name << std::endl;
+        ROS_INFO("%s", ros_node_name.c_str());
+        size_t pos = ros_node_name.find(node_namespace + '/');
+
+        if (pos == std::string::npos)
+        {
+            pos = ros_node_name.find(node_namespace);
+            ros_node_name.replace(pos, node_namespace.length(), "");
+            return ros_node_name;
+        }
+
+        ROS_INFO("namespace %s", node_namespace.c_str());
+        ros_node_name.replace(pos, node_namespace.length() + 1, "");
 
         return ros_node_name;
     }
